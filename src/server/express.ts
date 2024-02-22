5d9d35eda02f72c9215159f5d933081b283147a9
d-ito1106 <d_ito@taiho-pp.co.jp>
2024-02-22 11:52:38 +0900

    [add:test]get todo:running in express

--- src/server/express.ts
+++ src/server/express.ts
@@ -1,9 +1,21 @@
 import express from "express";
 
+//todo 
+import TodoTable from "../database/TodoTable";
+
 const app = express();
 
-app.get("/api/todos", (req, res) => {
-  res.send("こんにちは　せかい");
+export default app;
+
+app.get("/api/todos", async (req, res) => {
+  const db = new TodoTable();
+  const result = await db.selectAllTodos();
+  res.status(200).json(result);
 });
+app.get("/api/todos/running",async(req,res)=>{
+  const db = new TodoTable();
+  const result = await db.selectTodoByStatus("running");
+  res.status(200).json(result);
+})
 
 app.listen(3000);
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [react(),tsconfigPaths()],
   test:{
+    globals:true,
     include:['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
   }
 });
