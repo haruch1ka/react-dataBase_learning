28e5588cf28293701218daa0198e2862ea468fa3
d_ito <haruchikav@gmail.com>
2024-02-23 16:51:31 +0900

    add post fx

--- src/server/express.ts
+++ src/server/express.ts
@@ -1,16 +1,35 @@
 import express from "express";
 
 import TodoTable from "../database/TodoTable";
-
-
+import Todo from "../general/Todo";
 
 const app = express();
+app.use(express.json()); // body-parser settings
+
 app.get("/api/todos", async (req, res) => {
   const db = new TodoTable();
   const result = await db.selectAllTodos();
   res.status(200).json(result);
 });
 
-app.listen(3000);
+app.get("/api/todos/running", async (req, res) => {
+  const db = new TodoTable();
+  const result = await db.selectTodoByStatus("running");
+  res.status(200).json(result);
+});
+app.get("/api/todos/completed", async (req, res) => {
+  const db = new TodoTable();
+  const result = await db.selectTodoByStatus("completed");
+  res.status(200).json(result);
+});
+app.post("/api/todos", async (req, res) => {
+  console.log(req.body);
+  const { id, content, due_date, status } = req.body;
+  const todo = new Todo(id, content, due_date, status);
+  const db = new TodoTable();
+  await db.createTodo(todo);
+  res.status(200).json(todo);
+});
 
 export default app;
+app.listen(3000);
--- src/test/express.test.ts
+++ src/test/express.test.ts
@@ -8,6 +8,7 @@ import app from "../server/express";
 //Todo
 import TodoTable from "../database/TodoTable";
 import Todo from "../general/Todo";
+
 import { v4 as uuidv4 } from "uuid";
 
 const clearDB = new TodoTable();
@@ -37,4 +38,71 @@ it("The GET Method to the /api/todos can be got all todos.", async () => {
   //Asert
   expect(response.status).toEqual(200);
   expect(JSON.parse(response.text)).toEqual(todos);
+});
+
+it("The GET Method to the /api/todos/running can be got all running-todos.", async () => {
+  //Arrange
+  const id1 = uuidv4();
+  const id2 = uuidv4();
+  const id3 = uuidv4();
+  const id4 = uuidv4();
+  const db = new TodoTable();
+  const todos = [
+    new Todo(id1, "田中さんにメールする。", "20230201", "completed"),
+    new Todo(id2, "報告書を提出する。", "20230301", "completed"),
+    new Todo(id3, "会議を設定する。", "20230401", "running"),
+    new Todo(id4, "出張の準備をする。", "20230501", "running"),
+  ];
+  for (let i = 0; i < todos.length; i++) {
+    await db.createTodo(todos[i]);
+  }
+  // //Act
+  const response = await request(app).get("/api/todos/running").set("Accept", "application/json")
+  // //Asert
+  expect(response.status).toEqual(200);
+  expect(JSON.parse(response.text)).toEqual([todos[2], todos[3]]);
+});
+
+it("The GET Method to the /api/todos/completed can be got all completed-todos.", async () => {
+  //Arrange
+  const id1 = uuidv4();
+  const id2 = uuidv4();
+  const id3 = uuidv4();
+  const id4 = uuidv4();
+  const db = new TodoTable();
+  const todos = [
+    new Todo(id1, "田中さんにメールする。", "20230201", "completed"),
+    new Todo(id2, "報告書を提出する。", "20230301", "completed"),
+    new Todo(id3, "会議を設定する。", "20230401", "running"),
+    new Todo(id4, "出張の準備をする。", "20230501", "running"),
+  ];
+  for (let i = 0; i < todos.length; i++) {
+    await db.createTodo(todos[i]);
+  }
+
+  //Act
+  const response = await request(app).get("/api/todos/completed").set("Accept", "application/json");
+
+  //Asert
+  expect(response.status).toEqual(200);
+  expect(JSON.parse(response.text)).toEqual([todos[0], todos[1]]);
+});
+
+it("POST method to /api/todos can be created a todo." ,async ()=>{
+  //Arenge
+  const id = uuidv4();
+  const todo  =  new Todo(id,"会議を設定する","20230401","running");
+  const db = new TodoTable();
+
+  //Act
+  const response = await request(app)
+  .post("/api/todos")
+  .send(todo)
+  .set('Accept','application/json');
+
+  const selectedTodo = await db.selectTodoById(id);
+
+  //Assert
+  expect(response.status).toEqual(200);
+  expect(selectedTodo).toEqual(todo);
 });
