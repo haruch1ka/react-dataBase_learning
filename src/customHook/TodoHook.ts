90319e0e2a1cdea4005fdc0962601afd88cdf254
d-ito1106 <d_ito@taiho-pp.co.jp>
2024-02-28 11:14:32 +0900

    f

--- src/customHook/TodoHook.ts
+++ src/customHook/TodoHook.ts
@@ -1,14 +1,8 @@
 import { useState } from "react";
 
-<<<<<<< HEAD
 import Todo from "../general/Todo.ts";
 import { v4 as uuidv4 } from "uuid";
 import MyFetch from "../general/MyFetch";
-=======
-import MyFetch from "../general/MyFetch";
-import Todo from "../general/Todo";
-import { v4 as uuidv4 } from "uuid";
->>>>>>> e173d2bcd4abdac6ddece381fe3e89173ec734ef
 
 const TodoHook = () => {
   const [content, setContent] = useState<string>("");
